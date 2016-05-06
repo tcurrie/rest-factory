ec3a801327b7d675f9215a5ef0e0fdc8183b1032
curriet <tcurrie@cobaltgroup.com>
2016-05-06 12:27:25 -0700

    Add update deploy dir var.

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
-    mvn deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
+    mvn deploy --settings $DEPLOY/settings.xml -DperformRelease=true -DskipTests=true
     exit $?
 fi
 
