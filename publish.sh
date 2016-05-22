57bd39a3f083875130a0b3282822c5c984a44432
curriet <tcurrie@cobaltgroup.com>
2016-05-21 22:39:03 -0700

    Remove dependence on http servlet.  Re-implement options.

--- publish.sh
+++ publish.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_TAG == "" ]]; then
-    git config --global user.email "builds@travis-ci.com"
-    git config --global user.name "Travis CI"
-    git tag $VERSION -a -m "Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER"
-    git push origin --tags
-    mvn deploy --settings $DEPLOY/settings.xml -DskipTests=true -Prelease
+if [[ ${TRAVIS_PULL_REQUEST} == "false" && ${TRAVIS_TAG} == "" ]]; then
+    mvn deploy --settings ${DEPLOY}/settings.xml -DskipTests=true -Prelease
     exit $?
 fi
 
